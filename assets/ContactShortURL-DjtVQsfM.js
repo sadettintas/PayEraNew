5a6ea538a5fac17a65930e2c3f9f1b239bd90368
sadettintas <136625545+sadettintas@users.noreply.github.com>
2025-06-04 16:39:29 +0000

    Deploying to gh-pages from @ sadettintas/PayEraNew@465c0a3414ad4bcb5bc5d5b4932194afa65b0c43 🚀

---
+++ assets/ContactShortURL-DjtVQsfM.js
@@ -0,0 +1,2 @@
+import{j as t}from"./index-NA-1_KS0.js";import{S as r}from"./ShortUrlRedirect-DgtOGB8k.js";const o=()=>t.jsx(r,{targetPath:"/iletisim",targetName:"İletişim",delay:3e3});export{o as default};
+//# sourceMappingURL=ContactShortURL-DjtVQsfM.js.map
---
+++ assets/ShortURL-CLV2qTnj.js
@@ -0,0 +1,2 @@
+import{j as t}from"./index-NA-1_KS0.js";import{S as a}from"./ShortUrlRedirect-DgtOGB8k.js";const o=()=>t.jsx(a,{targetPath:"/hakkimizda",targetName:"Hakkımızda",delay:3e3});export{o as default};
+//# sourceMappingURL=ShortURL-CLV2qTnj.js.map
